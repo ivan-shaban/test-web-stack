1e529d811a9714edfb98cde04840e6d55296dea1
ishaban <ishaban.flash@gmail.com>
2021-12-08 19:20:32 +0300

    add page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type {AppProps} from 'next/app'
 import {useApollo} from '../lib/apollo'
 import {ApolloProvider} from '@apollo/client'
@@ -7,9 +8,14 @@ function MyApp({Component, pageProps}: AppProps) {
     const apolloClient = useApollo(pageProps)
 
     return (
-        <ApolloProvider client={apolloClient}>
-            <Component {...pageProps} />
-        </ApolloProvider>
+        <>
+            <Head>
+               <title>Test app: Nextjs + Prisma + GraphQL + SQLite</title>
+             </Head>
+            <ApolloProvider client={apolloClient}>
+                <Component {...pageProps} />
+            </ApolloProvider>
+        </>
     )
 }
 
