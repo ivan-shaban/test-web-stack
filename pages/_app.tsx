15eb429f36fb5dff30c02f5ca010dd1a48c9a027
ishaban <ishaban.flash@gmail.com>
2021-12-09 20:23:19 +0300

    fix mobile layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,8 @@ function MyApp({Component, pageProps}: AppProps) {
         <>
             <Head>
                <title>Test app: Nextjs + Prisma + GraphQL + SQLite</title>
+                <meta name="viewport"
+                      content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"/>
              </Head>
             <ApolloProvider client={apolloClient}>
                 <Component {...pageProps} />
