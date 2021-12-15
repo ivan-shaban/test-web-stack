49a4cc18a1c019c4833b46ffe839ee4653d569d5
ishaban <ishaban.flash@gmail.com>
2021-12-16 00:23:40 +0300

    remove prisma, apollo

--- src/lib/apis/graphql.ts
+++ src/lib/apis/graphql.ts
@@ -1,5 +1,3 @@
-import gql from 'graphql-tag'
-
 export const getAllUsers = /* GraphQL */ `
     query MyQuery {
         listUsers {
@@ -9,37 +7,6 @@ export const getAllUsers = /* GraphQL */ `
                 dob
                 name
             }
-        }
-    }
-`
-
-export const GET_ALL_USERS_QUERY = gql`
-    query Users($take: Int) {
-        users(take: $take) {
-            id
-            name
-            dob
-            address
-            image
-            description
-        }
-    }
-`
-
-export const UPDATE_USER_MUTATION = gql`
-    mutation Mutation($data: UserUpdateInput!, $where: UserWhereUniqueInput!) {
-        updateUser(data: $data, where: $where) {
-            name
-            address
-            description
-        }
-    }
-`
-
-export const DELETE_USER_MUTATION = gql`
-    mutation Mutation($where: UserWhereUniqueInput!) {
-        deleteUser(where: $where) {
-            id
         }
     }
 `
--- src/pages/users.ts
+++ src/pages/users.ts
@@ -3,16 +3,7 @@ import {
     UsersPage,
 } from '../components/pages/users/UsersPage'
 import {GetServerSideProps} from 'next'
-import {
-    addApolloState,
-    initializeApollo,
-} from '../lib/apollo'
-import {GET_ALL_USERS_QUERY} from '../lib/apis/graphql'
-import {User} from 'graphql/generated/type-graphql/models/User'
-import Amplify, {
-    API,
-    withSSRContext,
-} from 'aws-amplify'
+import Amplify, {withSSRContext} from 'aws-amplify'
 import awsconfig from '../aws-exports'
 import {GraphQLResult} from '@aws-amplify/api-graphql'
 import {ListUsersQuery} from '../API'
@@ -30,19 +21,6 @@ export const getServerSideProps: GetServerSideProps<Props, { page: string }> = a
         query: listUsers, variables: {},
     }) as Promise<GraphQLResult<ListUsersQuery>>)
     console.log(`>> users`, data);
-    // const apolloClient = initializeApollo()
-    //
-    // await apolloClient.query<{ users: User[] }, { take: number }>({
-    //     query: GET_ALL_USERS_QUERY,
-    //     variables: {
-    //         // @ts-ignore
-    //         take: pageIndex * process.env.NEXT_PUBLIC_USERS_PER_PAGE,
-    //     },
-    // })
-    //
-    // return addApolloState(apolloClient, {
-    //     props: {},
-    // })
 
     return {
         props: {
