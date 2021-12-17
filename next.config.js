ae579fb55ffa4d7cfada7a9bd17ebf083d9e14d3
ishaban <ishaban.flash@gmail.com>
2021-12-17 22:11:13 +0300

    change build directory to default

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    // split dev \ prod builds, because they may work at the same time
-    distDir: process.env.NODE_ENV === 'production'
-        ? 'build'
-        : '.next',
     eslint: {
         ignoreDuringBuilds: true,
     },
